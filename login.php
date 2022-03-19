667525181005a21d97c87cb5939ba1a90d7cfb05
Jeniffer-mg <jenidmedina31@hotmail.com>
2022-03-18 22:19:35 -0500

    Delete echos

--- login.php
+++ login.php
@@ -1,13 +1,13 @@
 <?php
 
-echo "get into php file";
 
-$dbhos = "localhost";
+$dbhost = "localhost";
 $dbuser = "root";
 $dbpass = "";
-$dbname = "db_videogames"
+$dbname = "db_videogames";
 
 $conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+
 if(!$conn){
     die("No hay conexion: ".mysqli_connect_error());
 }
@@ -17,11 +17,11 @@ $pass = $_POST["txtpassword"];
 
 $query = mysqli_query($conn, "SELECT * FROM  login WHERE usuario = '".$nombre."' and password = '".$pass."'");
 $nr = mysqli_num_rows($query);
-echo $nr;
 if($nr == 1){
     //header ("Location: index.html")
     echo "Bienvenid@: " .$nombre;
 } else if ($nr == 0){
     echo "No ingreso, usuario incorrecto";
 }
+
 ?>
